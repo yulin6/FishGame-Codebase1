4636a570be0febd29a616b571062a619f7ff87c8
lin.yu1@northeastern.edu <lin.yu1@husky.neu.edu>
2020-10-02 12:52:05 +0800

    refined C and E

--- C/Other/xjson/src/xjson/Main.java
+++ C/Other/xjson/src/xjson/Main.java
@@ -1,13 +1,10 @@
 package xjson;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonStreamParser;
 import java.util.Scanner;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 /** A main class for the program xjson.
  **/
@@ -19,139 +16,54 @@ public class Main {
    * @param args arbitrarily long sequence of JSON values.
    */
 
-  public static void main(String args[]) throws ParseException {
+  public static void main(String args[]) {
 
-    Scanner scanner = new Scanner(System.in);
-    JSONArray inputsAsJsonArray = new JSONArray();
-    JSONParser parser = new JSONParser();
 
-    Map<String, String> mapOfBracesOrBrackets = new HashMap<String, String>();
-    mapOfBracesOrBrackets.put("{", "}");
-    mapOfBracesOrBrackets.put("[", "]");
-    String[] arrayOfBooleanOrNull = new String[]{"true", "false", "null"};
-
+    Scanner  scanner = new Scanner(System.in);
+    StringBuilder builder = new StringBuilder();
+    JsonArray jsonArray = new JsonArray();
+    //JsonArray takes in JsonElements
+    JsonElement element;
 
     while (scanner.hasNextLine()) {
-      String currentLine = scanner.nextLine();
+      builder.append(scanner.nextLine());
 
-      for (String openingBracesOrBrackets : mapOfBracesOrBrackets.keySet()) {
-        currentLine = currentLine.replace(openingBracesOrBrackets,
-            " " + openingBracesOrBrackets);
-      }
-      for (String closingBracesOrBrackets : mapOfBracesOrBrackets.values()) {
-        currentLine = currentLine.replace(closingBracesOrBrackets,
-            closingBracesOrBrackets + " ");
-      }
-      for (String booleanOrNull : arrayOfBooleanOrNull) {
-        currentLine = currentLine.replace(booleanOrNull,
-            " " + booleanOrNull + " ");
-      }
-
-      String properSpacedLine = addSpaceForQuotes(currentLine);
-      currentLine = properSpacedLine;
-
-      Scanner rescanCurrentLine = new Scanner(currentLine);
-      while (rescanCurrentLine.hasNext()) {
-        String currentToken = rescanCurrentLine.next();
-        while (!isBracesOrBracketsCompleted(mapOfBracesOrBrackets, currentToken)) {
-          if (rescanCurrentLine.hasNext()) {
-            currentToken = currentToken + rescanCurrentLine.next();
-          }
+      // Handle the case when there is an empty line.
+      String trimmedString = builder.toString().trim();
+      if (!trimmedString.equals("")) {
+        //JsonStreamParser can't parse the entire string with line breaks,
+        //so we decided to parse it line by line.
+        JsonStreamParser parser = new JsonStreamParser(builder.toString());
+        while (parser.hasNext()) {
+          element = parser.next();
+          jsonArray.add(element);
         }
-        inputsAsJsonArray.add(parser.parse(currentToken));
+        //once the current line is scanned and parsed, set the string builder to empty.
+        builder = new StringBuilder();
       }
     }
-
-    printAnswers(inputsAsJsonArray);
+    generateAnswers(jsonArray);
 
   }
 
-  /** If there is a string with extra quotes inside of it, it will turn the quotes
-   * into spaces instead.
-   *
-   * @param str the input String that needs to be changed.
-   **/
-  public static String addSpaceForQuotes(String str) {
-    String strTemp = str;
-    boolean isRight = false;
+  public static void generateAnswers(JsonArray jsonArray){
+//    JsonElement jsonArraySize = new JsonPrimitive(jsonArray.size());
 
-    for (int i = 0; i < strTemp.length(); ++i) {
-      char character = strTemp.charAt(i);
-      if (character == '\"') {
+    int jsonArraySize = jsonArray.size();
+    JsonObject firstOutput = new JsonObject();
+    //addProperty() can take in primitive num unlike add() takes in json element
+    firstOutput.addProperty("count", jsonArraySize);
+    firstOutput.add("seq", jsonArray);
 
-        if (!isRight) {
-          if (i != 0) {
-            if (strTemp.charAt(i - 1) != ' ') {
-              String tmp1 = strTemp.substring(0, i);
-              String tmp2 = strTemp.substring(i);
-              strTemp = tmp1 + " " + tmp2;
-            }
-          }
-        } else {
-          if (i != strTemp.length() - 1) {
-            if (strTemp.charAt(i + 1) != ' ') {
-              String tmp1 = strTemp.substring(0, i + 1);
-              String tmp2 = strTemp.substring(i + 1);
-              strTemp = tmp1 + " " + tmp2;
-            }
-          }
-        }
-
-        isRight = !isRight;
-      }
+    JsonArray secondOutput = new JsonArray();
+    secondOutput.add(jsonArraySize);
+    for (int i = jsonArray.size() - 1; i >= 0; --i) {
+      secondOutput.add(jsonArray.get(i));
     }
-    return strTemp;
+
+    System.out.println(firstOutput);
+    System.out.println(secondOutput);
   }
 
-  /** Checks whether the map of json value that is within an array or list has
-   * a complete opening and closing bracket.
-   *
-   * @param mapOfBracesOrBrackets a map of Strings that are inside a bracket.
-   * @param str a string that could possibly be inside or outside the map.
-   * @return a boolean value that determines if the map is a complete json array or list.
-   **/
-
-  public static boolean isBracesOrBracketsCompleted(Map<String, String> mapOfBracesOrBrackets,
-      String str) {
-    for (Entry<String, String> e : mapOfBracesOrBrackets.entrySet()) {
-      String opening = e.getKey();
-      String closing = e.getValue();
-
-      int count1 = 0;
-      int index1 = 0;
-      while ((index1 = str.indexOf(opening, index1)) != -1) {
-        ++count1;
-        ++index1;
-      }
-      int count2 = 0;
-      int index2 = 0;
-      while ((index2 = str.indexOf(closing, index2)) != -1) {
-        ++count2;
-        ++index2;
-      }
-
-      if (count1 != count2) return false;
-    }
-    return true;
-  }
-
-  /** Prints the JSONArray into the console.
-   *
-   * @param jArray JSONArray of JSON values.
-   */
-
-  public static void printAnswers(JSONArray jArray) {
-    JSONObject firstAnswer = new JSONObject();
-    firstAnswer.put("count", jArray.size());
-    firstAnswer.put("seq", jArray);
-
-    JSONArray secondAnswer = new JSONArray();
-    for (int i = 0; i < jArray.size(); ++i) secondAnswer.add(0, jArray.get(i));
-
-    secondAnswer.add(0, jArray.size());
-
-    System.out.println(firstAnswer);
-    System.out.println(secondAnswer);
-  }
 
 }
--- E/Other/xtcp/src/main/XtcpServer.java
+++ E/Other/xtcp/src/main/XtcpServer.java
@@ -13,14 +13,16 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-/** An XtcpServer is a class that consumes a well-formed JSON values
- * from the input side of a TCP connection and delivers JSON to the output side of a TCP
- * connection after the input side is closed.
+/**
+ * An XtcpServer is a class that consumes a well-formed JSON values from the input side of a TCP
+ * connection and delivers JSON to the output side of a TCP connection after the input side is
+ * closed.
  **/
 
 public class XtcpServer {
 
-  /** A main class that communicates with linux server through inputting a server.
+  /**
+   * A main class that communicates with linux server through inputting a server.
    *
    * @param args a server port number.
    **/
@@ -28,15 +30,16 @@ public class XtcpServer {
 
     int serverPort = 4567;
     if (args.length > 1) {
+      //System.out.println in the code are alerts for the server side.
       System.out.println("Invalid input arguments, "
-          + "please input one port number between 0 to 65535 inclusively.");
+          + "please input one port number between 1 to 65535 inclusively.");
       return;
     } else if (args.length == 1) {
       if (isValidPortNum(args[0])) {
         serverPort = Integer.parseInt(args[0]);
       } else {
         System.out.println("Invalid input argument, "
-            + "please input one port number between 0 to 65535 inclusively.");
+            + "please input one port number between 1 to 65535 inclusively.");
         return;
       }
     }
@@ -45,21 +48,26 @@ public class XtcpServer {
 
   }
 
-  /** serverOperations takes in a serverPort and creates a connection from client that stores
-   * the clientInput and serverOutput data and then outputs a JSON values to the console based on
-   * the clientInput.
+  /**
+   * serverOperations takes in a serverPort and creates a connection from client that stores the
+   * clientInput and serverOutput data and then outputs a JSON values to the console based on the
+   * clientInput.
    *
    * @param serverPort the server port number.
    **/
   public static void serverOperations(int serverPort) {
+    int timeoutNum = 3000;
     try {
-      ServerSocket server = new ServerSocket(serverPort);
+      ServerSocket server = new ServerSocket(serverPort); // throws IOException
       System.out.println("*** Server running, current port is: " + serverPort + " ***");
       System.out.println("Waiting for client to connect...");
-      server.setSoTimeout(3000); //Setting timeout if no client is connecting in 3 seconds.
+      server.setSoTimeout(timeoutNum); //Setting timeout if no client is connecting in 3 seconds.
 
       Socket clientConnection = server.accept(); //accept & create connection from client.
+
+      // Reading data from client's input stream
       DataInputStream clientInput = new DataInputStream(clientConnection.getInputStream());
+      // Writing data to the client's output stream
       DataOutputStream serverOutput = new DataOutputStream(clientConnection.getOutputStream());
 
       System.out.println("Client connected.");
@@ -75,60 +83,74 @@ public class XtcpServer {
     }
   }
 
-  /** isValidPortNum determines whether a string is a valid server port number and check whether
-   * its and integer and its values are between 0 to 65535.
+  /**
+   * isValidPortNum determines whether a string is a valid server port number and check whether its
+   * and integer and its values are between 0 to 65535.
    *
    * @param s server port number from console input.
    * @return a boolean value to determine if the port number is valid.
    **/
   public static boolean isValidPortNum(String s) {
     int i;
+    int startNum = 1;
+    int endNum = 65535;
     try {
       i = Integer.parseInt(s);
     } catch (NumberFormatException e) {
       return false;
     }
-    return i > 0 && i <= 65535;
+    return i >= startNum && i <= endNum;
   }
 
-  /** generateJsonArray builds a JsonArray object based on the parsed data that is scanned from
-   * the user file.
+  /**
+   * generateJsonArray builds a JsonArray object based on the parsed data that is scanned from the
+   * user file.
    *
    * @param scanner is a scanned values from the user file.
    **/
   public static JsonArray generateJsonArray(Scanner scanner) {
     StringBuilder builder = new StringBuilder();
     JsonArray jsonArray = new JsonArray();
+    //JsonArray takes in JsonElements
     JsonElement element;
 
     while (scanner.hasNextLine()) {
       builder.append(scanner.nextLine());
-      // When the input of client user is not empty.
-      if (!builder.toString().trim().equals("")) {
+
+      // Handle the case when there is an empty line.
+      String trimmedString = builder.toString().trim();
+      if (!trimmedString.equals("")) {
+        //JsonStreamParser can't parse the entire string with line breaks,
+        //so we decided to parse it line by line.
         JsonStreamParser parser = new JsonStreamParser(builder.toString());
         while (parser.hasNext()) {
           element = parser.next();
           jsonArray.add(element);
         }
+        //once the current line is scanned and parsed, set the string builder to empty.
         builder = new StringBuilder();
       }
     }
     return jsonArray;
   }
 
-  /** generateOutputs fills the DataOutputStream with the populated JsonArray into
-   * the appropriate JSON output.
+  /**
+   * generateOutputs fills the DataOutputStream with the populated JsonArray into the appropriate
+   * JSON output.
    *
    * @param jsonArray populated JsonArray from the clientInputData.
    * @param serverOutput an empty DataOutputStream.
    **/
   public static void generateOutputs(JsonArray jsonArray, DataOutputStream serverOutput) {
-    JsonElement jsonArraySize = new JsonPrimitive(jsonArray.size());
+//    JsonElement jsonArraySize = new JsonPrimitive(jsonArray.size());
 
+    int jsonArraySize = jsonArray.size();
     JsonObject firstOutput = new JsonObject();
-    firstOutput.add("count", jsonArraySize);
+    //addProperty() can take in a primitive num while add() takes in a json element
+    firstOutput.addProperty("count", jsonArraySize);
     firstOutput.add("seq", jsonArray);
 
+    //reverse the array
     JsonArray secondOutput = new JsonArray();
     secondOutput.add(jsonArraySize);
     for (int i = jsonArray.size() - 1; i >= 0; --i) {
@@ -136,7 +158,8 @@ public class XtcpServer {
     }
 
     try {
-      serverOutput.writeChars(firstOutput.toString());
+      //writing chars to DataOutputStream
+      serverOutput.writeChars(firstOutput.toString()); //throws IOException
       serverOutput.writeChars("\n");
       serverOutput.writeChars(secondOutput.toString());
       serverOutput.writeChars("\n");
